2421ad1923137895e927c5af2f4d2a27415e5072
yousef <yousefshwaly@gmail.com>
2025-09-22 12:05:19 +0300

    Add tailwind-preitter-extension

--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -1,7 +1,7 @@
 function Home() {
   return (
     <div>
-      <h1>
+      <h1 className="m-5 p-5 text-center text-3xl font-semibold text-yellow-500">
         The best pizza.
         <br />
         Straight out of the oven, straight to you.
