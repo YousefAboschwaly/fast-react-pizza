0b4914762d2437c96a0aa136500aab35bf60c1af
yousef <yousefshwaly@gmail.com>
2025-09-23 16:22:49 +0300

    Using Grid in AppLayout

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -4,16 +4,18 @@ import { Outlet, useNavigation } from "react-router-dom";
 import Loader from "./Loader";
 
 export default function AppLayout() {
-  const navigation = useNavigation()
-  const isLoading = navigation.state === "loading"
+  const navigation = useNavigation();
+  const isLoading = navigation.state === "loading";
   return (
-    <div className="layout">
-      {isLoading && <Loader/>}
+    <div className="grid h-screen grid-rows-[auto_1fr_auto]">
+      {isLoading && <Loader />}
       <Header />
 
-      <main>
-        <Outlet />
-      </main>
+      <div className="overflow-y-scroll">
+        <main className="mx-auto max-w-3xl  ">
+          <Outlet />
+        </main>
+      </div>
 
       <CartOverview />
     </div>
