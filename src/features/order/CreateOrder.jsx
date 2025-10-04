3cb7e3604af1d46e395b967fdc79c07f4d26f44c
yousef <yousefshwaly@gmail.com>
2025-10-04 16:12:45 +0300

    Modeling the Cart State

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -20,7 +20,7 @@ const fakeCart = [
   },
   {
     pizzaId: 6,
-    name: "Vegetale",
+    name: "Vegetate",
     quantity: 1,
     unitPrice: 13,
     totalPrice: 13,
@@ -35,7 +35,7 @@ const fakeCart = [
 ];
 
 function CreateOrder() {
-      const userName = useSelector((store)=>store.user.userName)
+  const userName = useSelector((store) => store.user.userName);
 
   const navigation = useNavigation();
   const isSubmitting = navigation.state === "submitting";
@@ -46,46 +46,58 @@ function CreateOrder() {
 
   return (
     <div className="px-4 py-6">
-      <h2 className="text-xl font-semibold mb-8">Ready to order? Let's go!</h2>
+      <h2 className="mb-8 text-xl font-semibold">Ready to order? Let's go!</h2>
 
       <Form method="POST">
-        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center ">
-          <label  className="sm:basis-40">First Name</label>
+        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+          <label className="sm:basis-40">First Name</label>
           <div className="flex-1">
-            <input type="text" name="customer" required className="input" defaultValue={userName} />
+            <input
+              type="text"
+              name="customer"
+              required
+              className="input"
+              defaultValue={userName}
+            />
           </div>
         </div>
 
-        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center ">
+        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
           <label className="sm:basis-40">Phone number</label>
-          <div  className="flex-1">
+          <div className="flex-1">
             <input type="tel" name="phone" required className="input" />
-          {formErrors?.phone && <p className="text-xs bg-red-100 text-red-700 mt-2 p-2 rounded-md">{formErrors.phone}</p>}
+            {formErrors?.phone && (
+              <p className="mt-2 rounded-md bg-red-100 p-2 text-xs text-red-700">
+                {formErrors.phone}
+              </p>
+            )}
           </div>
         </div>
 
-        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center ">
+        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
           <label className="sm:basis-40">Address</label>
-          <div  className="flex-1">
+          <div className="flex-1">
             <input type="text" name="address" required className="input" />
           </div>
         </div>
 
         <div className="mb-12 flex items-center gap-3">
           <input
-            className="h-6 w-6 accent-yellow-400 focus:ring focus:ring-yellow-400 focus:ring-offset-2 focus:outline-none hover:cursor-pointer"
+            className="h-6 w-6 accent-yellow-400 hover:cursor-pointer focus:ring focus:ring-yellow-400 focus:ring-offset-2 focus:outline-none"
             type="checkbox"
             name="priority"
             id="priority"
             // value={withPriority}
             // onChange={(e) => setWithPriority(e.target.checked)}
           />
-          <label className="font-medium" htmlFor="priority">Want to yo give your order priority?</label>
+          <label className="font-medium" htmlFor="priority">
+            Want to yo give your order priority?
+          </label>
         </div>
         <input type="hidden" name="cart" value={JSON.stringify(cart)} />
 
         <div>
-          <Button disabled={isSubmitting} type={"primary"} >
+          <Button disabled={isSubmitting} type={"primary"}>
             {isSubmitting ? "Placing Order..." : "Order now"}
           </Button>
         </div>
