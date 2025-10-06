2a8121530377cde9803d0d2c095c5af0dbff09bf
yousef <yousefshwaly@gmail.com>
2025-10-06 11:55:37 +0300

    using ingradients of each pizzaOrder getting it by useFetcher

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react-refresh/only-export-components */
 // Test ID: IIDSAT
 
-import { useLoaderData } from "react-router-dom";
+import { useFetcher, useLoaderData } from "react-router-dom";
 import { getOrder } from "../../services/apiRestaurant";
 import {
   calcMinutesLeft,
@@ -9,12 +9,21 @@ import {
   formatDate,
 } from "../../utils/helpers";
 import OrderItem from './OrderItem';
+import { useEffect } from "react";
 
 function Order() {
   // Everyone can search for all orders, so for privacy reasons we're gonna gonna exclude names or address, these are only for the restaurant staff
   
   const order = useLoaderData();
-  console.log(order);
+
+const fetcher = useFetcher()
+useEffect(()=>{
+  if(!fetcher.data && fetcher.state == 'idle'){
+    fetcher.load("/menu")
+  }
+},[fetcher])
+console.log(fetcher.data)
+
   const {
     id,
     status,
@@ -52,7 +61,7 @@ function Order() {
 
 
       <ul className="divide-y divide-stone-200 border-b border-t border-stone-200">
-        {cart.map(item=><OrderItem item={item} key={item.pizzaId}/>)}
+        {cart.map(item=><OrderItem item={item} key={item.pizzaId} ingredients={fetcher.data?.find(el=>el.id === item.pizzaId)?.ingredients} isLoadingIngredients={fetcher.state === "loading"}/>)}
       </ul>
 
 
