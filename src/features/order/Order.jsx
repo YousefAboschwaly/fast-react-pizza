cfb70a2db9e4a59a2a1dc3b39249b02f8dc0bd7b
yousef <yousefshwaly@gmail.com>
2025-10-06 15:39:17 +0300

    Make UpdateOrder component

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -10,6 +10,7 @@ import {
 } from "../../utils/helpers";
 import OrderItem from './OrderItem';
 import { useEffect } from "react";
+import UpdateOrder from "./UpdateOrder";
 
 function Order() {
   // Everyone can search for all orders, so for privacy reasons we're gonna gonna exclude names or address, these are only for the restaurant staff
@@ -70,6 +71,7 @@ console.log(fetcher.data)
         {priority && <p  className="text-sm font-medium text-stone-600">Price priority: {formatCurrency(priorityPrice)}</p>}
         <p className="font-bold">To pay on delivery: {formatCurrency(orderPrice + priorityPrice)}</p>
       </div>
+     {!priority && <UpdateOrder order={order}/>}
     </div>
   );
 }
---
+++ src/features/order/UpdateOrder.jsx
@@ -0,0 +1,8 @@
+import Button from "./../../ui/Button";
+export default function UpdateOrder({ order }) {
+  return (
+    <div className="text-right">
+      <Button type={"primary"}>Make priority</Button>
+    </div>
+  );
+}
