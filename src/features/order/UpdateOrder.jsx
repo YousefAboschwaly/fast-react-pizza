7cf6bc304e16dcf5d2c66e73136da264e2c2ba58
yousef <yousefshwaly@gmail.com>
2025-10-06 15:55:43 +0300

    Updating Data Without Navigation

--- src/features/order/UpdateOrder.jsx
+++ src/features/order/UpdateOrder.jsx
@@ -1,8 +1,20 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react-refresh/only-export-components */
+import { useFetcher } from "react-router-dom";
 import Button from "./../../ui/Button";
+import { updateOrder } from "../../services/apiRestaurant";
 export default function UpdateOrder({ order }) {
+  const fetcher = useFetcher()
   return (
-    <div className="text-right">
+    <fetcher.Form  method="PATCH" className="text-right">
       <Button type={"primary"}>Make priority</Button>
-    </div>
+    </fetcher.Form>
   );
+}
+
+export async function action({params}){
+console.log("update")
+const data = {priority:true}
+await updateOrder(params.orderId,data)
+return null
 }
